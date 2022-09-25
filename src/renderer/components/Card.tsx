b8f6ce99f94fb89d6ee8db5aead3eaafabec6a84
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-09-25 11:36:48 +0200

    refractor: Cards

---
+++ src/renderer/components/Card.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Card({ icon, link, title }: CardProps) {
+
+  return (
+    <div className="card">
+      <Link to={link}>
+        <img src={icon} alt="" />
+        <h2 className="card-title">{title}</h2>
+      </Link>
+    </div>
+  )
+}
+
+interface CardProps {
+  icon: string,
+  link: string,
+  title: string
+}
--- src/renderer/components/Homepage.tsx
+++ src/renderer/components/Homepage.tsx
@@ -1,32 +1,16 @@
-import { Link } from 'react-router-dom';
+import Card from './Card';
 import icon from './icon.png';
 
 export default function Homepage() {
   return (
     <div className="homepage">
       <div className="row">
-        <div className="card">
-          <Link to="/">
-            <img src={icon} alt="" />
-          </Link>
-        </div>
-        <div className="card">
-          <Link to="/">
-            <img src={icon} alt="" />
-          </Link>
-        </div>
+        <Card icon={icon} link="/" title='Card Title' />
+        <Card icon={icon} link="/" title='Card Title' />
       </div>
       <div className="row">
-        <div className="card">
-          <Link to="/">
-            <img src={icon} alt="" />
-          </Link>
-        </div>
-        <div className="card">
-          <Link to="/">
-            <img src={icon} alt="" />
-          </Link>
-        </div>
+        <Card icon={icon} link="/" title='Card Title' />
+        <Card icon={icon} link="/" title='Card Title' />
       </div>
     </div>
   );
