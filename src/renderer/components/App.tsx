24c7677db2e9a20ba9188eea140c1f437b4e9a24
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-09-25 12:36:58 +0200

    feat: add link to header title

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,11 +1,11 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './Homepage';
 
 export default function App() {
   return (
     <Router>
-      <header className="header">Folder Utils App</header>
+      <header className="header"><Link to="/">Folder Utils App</Link></header>
       <main className="container">
         <Routes>
           <Route path="/" element={<Homepage />} />
