44b22127111c36618a4350d072035c73f95f526f
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-09-25 13:00:14 +0200

    feat: add alt to imgs

--- src/renderer/components/Card.tsx
+++ src/renderer/components/Card.tsx
@@ -5,7 +5,7 @@ export default function Card({ icon, link, title }: CardProps) {
   return (
     <div className="card">
       <Link to={link}>
-        <img src={icon} alt="" />
+        <img src={icon} alt={title+" image"} />
         <h2 className="card-title">{title}</h2>
       </Link>
     </div>
