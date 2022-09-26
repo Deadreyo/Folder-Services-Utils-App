153ceeeb829fe8dd66b6e074cac230662bc51590
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-09-26 14:19:56 +0200

    feat: Folder Info data display

--- src/renderer/components/FolderInfoComponent.tsx
+++ src/renderer/components/FolderInfoComponent.tsx
@@ -10,7 +10,14 @@ export default function FolderInfo({ changeAction }: FolderInfoProps) {
     window.electron.ipcRenderer.sendMessage(FolderInfoChannel, [path])
 
     window.electron.ipcRenderer.once(FolderInfoChannel, (folderCount,  fileCount, oldest5Files, biggest5Files) => {
-      console.log(oldest5Files)
+      const obj: InfoData = {
+        folderCount: folderCount as number,
+        fileCount: fileCount as number,
+        oldest5Files: oldest5Files as oldFile[],
+        biggest5Files: biggest5Files as bigFile[]
+      }
+
+      setData(obj);
     })
 
   }
@@ -19,13 +26,77 @@ export default function FolderInfo({ changeAction }: FolderInfoProps) {
     changeAction(action)
   }, [])
 
-  // if (!data) return null;
+  if (!data) return null;
 
   return (
     <div className="page-inner-container">
       <div className="title-container">
         <h2>Info</h2>
       </div>
+
+      <table>
+        <tr>
+          <th>File Count:</th>
+          <td>{data.fileCount}</td>
+        </tr>
+        <tr>
+          <th>Folder Count:</th>
+          <td>{data.folderCount}</td>
+        </tr>
+      </table>
+
+      <table>
+        <caption>Biggest Files</caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Path</th>
+            <th>Size (MB)</th>
+          </tr>
+        </thead>
+        <tbody>
+        {data.biggest5Files.map( file => (
+          <tr key={file.path}>
+            <td>{file.name}</td>
+            <td>{file.path}</td>
+            <td>{file.size}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      <table>
+        <tr>
+          <th>File Count:</th>
+          <td>{data.fileCount}</td>
+        </tr>
+        <tr>
+          <th>Folder Count:</th>
+          <td>{data.folderCount}</td>
+        </tr>
+      </table>
+
+      <table>
+        <caption>Oldest Files</caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Path</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+        {data.oldest5Files.map( file => (
+          <tr key={file.path}>
+            <td>{file.name}</td>
+            <td>{file.path}</td>
+            <td>{file.date.toLocaleString()}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+
     </div>
   )
 }
